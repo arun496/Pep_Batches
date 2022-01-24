87c87ec25d964c6d7f35e056a1f1550b8478d89a
Arun <arun8778451315@gmail.com>
2022-01-25 00:00:36 +0530

    Update

--- FJP_ENG/2_JavaScript/File_System_Organizer/activity/commands/view.js
+++ FJP_ENG/2_JavaScript/File_System_Organizer/activity/commands/view.js
@@ -1,5 +1,32 @@
-function viewFn() {
+let fs = require("fs");
+let path = require("path");
+
+function viewFn(src, mode) {
     console.log("Viewing your folder");
+    viewHelperFlat(src);
+}
+
+function checkFileOrFolder(path) {
+    let isFile = fs.lstatSync(path).isFile();
+    return isFile;
+}
+
+function viewHelperFlat(src) {
+    // Is it a file or folder
+    let isFile = checkFileOrFolder(src);
+    if (isFile) {  // Base case
+        console.log(src, "*");
+    } else { // Folder => Folder1, Files
+        let childrens = fs.readdirSync(src); //Immediate Level childrens
+        
+        for (let i = 0;i < childrens.length;i++) {
+            let child = childrens[i];
+            let childPath = path.join(src, child);
+            console.log(childPath);
+
+            // Faith
+        }
+    }
 }
 
 module.exports = {
--- FJP_ENG/2_JavaScript/File_System_Organizer/activity/mycli.js
+++ FJP_ENG/2_JavaScript/File_System_Organizer/activity/mycli.js
@@ -2,13 +2,12 @@ let helpFn = require("./commands/help");
 let organizeFn = require("./commands/organize");
 let viewFn = require("./commands/view");
 
-let arr = process.argv.slice(2);
-
+let input = process.argv.slice(2);
 let command = input[0];
 
 switch(command) {
     case "view":
-        viewFn.view();
+        viewFn.view(input[1], input[2]);
         break;
     case "organize":
         organizeFn.organize();
