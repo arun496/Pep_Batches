0f321960b63de5583eb882144cc20c46fe2d14e3
Arun <arun8778451315@gmail.com>
2022-01-28 21:48:17 +0530

    Update

--- FJP_ENG/2_JavaScript/File_System_Organizer/activity/commands/view.js
+++ FJP_ENG/2_JavaScript/File_System_Organizer/activity/commands/view.js
@@ -2,8 +2,12 @@ let fs = require("fs");
 let path = require("path");
 
 function viewFn(src, mode) {
-    console.log("Viewing your folder");
-    viewHelperFlat(src);
+    // console.log("Viewing your folder");
+
+    if (mode == "flat")
+        viewHelperFlat(src);
+    else 
+        viewHelperTree(src, "|");
 }
 
 function checkFileOrFolder(path) {
@@ -11,20 +15,45 @@ function checkFileOrFolder(path) {
     return isFile;
 }
 
+// Print entire Path
 function viewHelperFlat(src) {
     // Is it a file or folder
     let isFile = checkFileOrFolder(src);
     if (isFile == true) {  // Base case
         console.log(src, "*");
     } else { // Folder => Folder1, Files
+        console.log(src);
         let childrens = fs.readdirSync(src); // Read content Immediate Level childrens
         
         for (let i = 0;i < childrens.length;i++) {
             let child = childrens[i]; // Not a complete path, just file or folder name
             let childPath = path.join(src, child);
-            console.log(childPath);
+            // console.log(childPath);
 
             // Faith ??
+            viewHelperFlat(childPath);
+            
+        }
+    }
+}
+
+// Print the path's foldername or filename
+function viewHelperTree(src, indent) {
+    // Is it a file or folder
+    let isFile = checkFileOrFolder(src);
+    if (isFile == true) {  // Base case
+        console.log(indent, path.basename(src), "*");  //path.basename() => F/App/index.js => index.js
+    } else { // Folder => Folder1, Files
+        console.log(indent, path.basename(src));
+        let childrens = fs.readdirSync(src); // Read content Immediate Level childrens
+        
+        for (let i = 0;i < childrens.length;i++) {
+            let child = childrens[i]; // Not a complete path, just file or folder name
+            let childPath = path.join(src, child);
+            // console.log(childPath);
+
+            // Faith ??
+            viewHelperTree(childPath, indent+"____");
             
         }
     }
